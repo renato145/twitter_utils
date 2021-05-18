516dd1bcfaaab41dec57fab405269d33661283d5
Renato <renato145@hotmail.com>
2021-05-18 21:06:04 +0930

    add heap limit

--- twitter_stream/run_elastic_search.sh
+++ twitter_stream/run_elastic_search.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker run -d --rm --name elasticsearch -v ${PWD}/elasticsearch_data:/usr/share/elasticsearch/data -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:7.12.1
+docker run -d --rm --name elasticsearch \
+    -v ${PWD}/elasticsearch_data:/usr/share/elasticsearch/data \
+    -p 9200:9200 -p 9300:9300 \
+    -e "discovery.type=single-node" \
+    -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
+    elasticsearch:7.12.1
